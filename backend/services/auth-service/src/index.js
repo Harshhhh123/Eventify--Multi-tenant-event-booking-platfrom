74660900013ce4b93ec0643f15552a26339446e7
Harshhhh123 <harshgoilkar27@gmail.com>
2026-02-10 15:37:45 +0530

    testing the cicd pipeline for uath service to ecs

--- backend/services/auth-service/src/index.js
+++ backend/services/auth-service/src/index.js
@@ -23,7 +23,7 @@ console.log("JWT_SECRET:", process.env.JWT_SECRET);
    HEALTH CHECK
 ----------------------------- */
 app.get("/health", (req, res) => {
-  res.json({ status: "auth-service running on port 4000 okkkkk" });
+  res.json({ status: "auth-service running " });
 });
 
 /* -----------------------------
@@ -197,5 +197,5 @@ console.log("LOGIN USER:", {
 ----------------------------- */
 const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`Auth service running on port here now lesgssgo cicd ${PORT}`);
+  console.log(`Auth service running on port ${PORT}`);
 });
