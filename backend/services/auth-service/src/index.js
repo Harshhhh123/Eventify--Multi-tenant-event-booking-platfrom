823d0cc6bbce7e49f633e68313cad229c36c56c1
Harshhhh123 <harshgoilkar27@gmail.com>
2026-02-10 13:52:18 +0530

    n

--- backend/services/auth-service/src/index.js
+++ backend/services/auth-service/src/index.js
@@ -48,7 +48,7 @@ app.post("/request-organizer", authenticate, async (req, res) => {
     );
 
     return res.json({
-      message: "Upgraded to organizer in DB"
+      message: "Upgraded to organizer successfully"
     });
 
   } catch (err) {
